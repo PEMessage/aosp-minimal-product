e0e67081b1151528ea38058ecb5fcce25f4ddfc2
PEMessage <1165739182@qq.com>
2026-08-17 01:25:16 +0800

    init: add android base env

--- flake.nix
+++ flake.nix
@@ -10,8 +10,52 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [ pkgs.hello ];
-      };
+      devShells.${system}.default = (pkgs.buildFHSEnv {
+        name = "aosp-env";
+        targetPkgs = pkgs: with pkgs; [
+          # Version Control
+          git
+          git-repo
+
+          # Build System and Code Generation
+          gnumake
+          m4
+          bison
+          flex
+          gperf
+
+          # System Administration and Performance
+          procps
+          nettools
+          schedtool
+          util-linux
+
+          # Network, Security, and Synchronization
+          curl
+          openssl
+          gnupg
+          rsync
+
+          # Compression Tools
+          zip
+          unzip
+          lzop
+
+          # Programming Languages and Runtimes
+          python3
+          perl
+          jdk8
+
+          # Development Libraries
+          libxml2
+        ];
+
+        profile = ''
+          export USE_CCACHE=1
+          export ANDROID_JAVA_HOME=${pkgs.jdk8.home}
+          export JAVA_HOME=${pkgs.jdk8.home}
+        '';
+
+      }).env;
     };
 }
